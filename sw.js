a8105ce8ca4a8bfa833b45f2393720efc850c07b
bombyul1011 <faith_003@naver.com>
2026-08-19 00:35:28 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // 채움로그 서비스워커
 // 배포 시 CACHE_VERSION만 올리면 이전 캐시가 자동 무효화됨 (iikoto와 동일 패턴)
-const CACHE_VERSION = 'chaeumlog-v20260819-08-tabbar-viewport-fix';
+const CACHE_VERSION = 'chaeumlog-v20260819-10-DEBUG-tabbar-diagnostic';
 const CORE_ASSETS = [
   './',
   './index.html',
